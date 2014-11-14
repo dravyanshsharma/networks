58dfe3b851a3bfa3e4d5ab5213a3462e697565e2
Akhil Jain <cs5110274@baadalvm34.cse.iitd.ac.in>
2014-11-15 00:29:22 +0530

    completed uplink and downlink buffer capacity estimation

--- udptests/runclient.sh
+++ udptests/runclient.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
 bash build.sh
-java -cp "lib/java-getopt-1.0.13.jar:lib/commons-collections-3.2.1.jar:lib/log4j-1.2.16.jar:lib/expiringcache.jar:build" udpprobe.client.UDPClient
+java -cp "lib/java-getopt-1.0.13.jar:lib/commons-collections-3.2.1.jar:lib/log4j-1.2.16.jar:lib/expiringcache.jar:build" udpprobe.client.downlink
+java -cp "lib/java-getopt-1.0.13.jar:lib/commons-collections-3.2.1.jar:lib/log4j-1.2.16.jar:lib/expiringcache.jar:build" udpprobe.client.uplink
+
