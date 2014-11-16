f95ad026c3d8d127dee16289ef6937b66e97df5d
Akhil Jain <nayaki.cse.iitd.ac.in>
2014-11-16 15:52:54 +0530

    edit the runclient script

--- udptests/runclient.sh
+++ udptests/runclient.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 bash build.sh
-#java -cp "lib/java-getopt-1.0.13.jar:lib/commons-collections-3.2.1.jar:lib/log4j-1.2.16.jar:lib/expiringcache.jar:build" udpprobe.client.downlink_q1
-#java -cp "lib/java-getopt-1.0.13.jar:lib/commons-collections-3.2.1.jar:lib/log4j-1.2.16.jar:lib/expiringcache.jar:build" udpprobe.client.downlink_q4a
-#java -cp "lib/java-getopt-1.0.13.jar:lib/commons-collections-3.2.1.jar:lib/log4j-1.2.16.jar:lib/expiringcache.jar:build" udpprobe.client.downlink_q4b
-#java -cp "lib/java-getopt-1.0.13.jar:lib/commons-collections-3.2.1.jar:lib/log4j-1.2.16.jar:lib/expiringcache.jar:build" udpprobe.client.uplink_q1
-#java -cp "lib/java-getopt-1.0.13.jar:lib/commons-collections-3.2.1.jar:lib/log4j-1.2.16.jar:lib/expiringcache.jar:build" udpprobe.client.uplink_q4a
+java -cp "lib/java-getopt-1.0.13.jar:lib/commons-collections-3.2.1.jar:lib/log4j-1.2.16.jar:lib/expiringcache.jar:build" udpprobe.client.downlink_q1
+java -cp "lib/java-getopt-1.0.13.jar:lib/commons-collections-3.2.1.jar:lib/log4j-1.2.16.jar:lib/expiringcache.jar:build" udpprobe.client.downlink_q4a
+java -cp "lib/java-getopt-1.0.13.jar:lib/commons-collections-3.2.1.jar:lib/log4j-1.2.16.jar:lib/expiringcache.jar:build" udpprobe.client.downlink_q4b
+java -cp "lib/java-getopt-1.0.13.jar:lib/commons-collections-3.2.1.jar:lib/log4j-1.2.16.jar:lib/expiringcache.jar:build" udpprobe.client.uplink_q1
+java -cp "lib/java-getopt-1.0.13.jar:lib/commons-collections-3.2.1.jar:lib/log4j-1.2.16.jar:lib/expiringcache.jar:build" udpprobe.client.uplink_q4a
 java -cp "lib/java-getopt-1.0.13.jar:lib/commons-collections-3.2.1.jar:lib/log4j-1.2.16.jar:lib/expiringcache.jar:build" udpprobe.client.uplink_q4b
